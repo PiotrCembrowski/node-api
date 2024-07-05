9d2ce40b2db9362977b5f8a0b927c553e505eaad
Piotr Cembrowski <losttape@gmail.com>
2024-07-05 14:43:06 +0200

    user/:id added

--- src/users/user.routes.ts
+++ src/users/user.routes.ts
@@ -96,3 +96,30 @@ userRouter.post("/login", async (req: Request, res: Response) => {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
   }
 });
+
+userRouter.put("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const getUser = await database.findOne(req.params.id);
+
+    if (!username || !email || !password) {
+      return res
+        .status(401)
+        .json({ error: "Please provide all the required parameters" });
+    }
+
+    if (!getUser) {
+      return res
+        .status(404)
+        .json({ error: `No user with id ${req.params.id}` });
+    }
+
+    const updateUser = await database.update(req.params.id, req.body);
+
+    return res.status(201).json({ updateUser });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+});
